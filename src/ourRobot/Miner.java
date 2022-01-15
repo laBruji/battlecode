af80c09b58093677e9bfff92067c2d0bc3f8ec5e
Sabrina Romero <sabrinaromero@Sabrinas-MacBook-Pro.local>
2022-01-15 01:45:00 -0500

    watchtowers and miners

--- src/ourRobot/Miner.java
+++ src/ourRobot/Miner.java
@@ -5,6 +5,7 @@ import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
+// TODO: Review Miner; too sleepy now
 public class Miner {
     /**
      * Run a single turn for a Miner.
@@ -16,21 +17,42 @@ public class Miner {
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
                 MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
-                // Notice that the Miner's action cooldown is very low.
-                // You can mine multiple times per turn!
                 while (rc.canMineGold(mineLocation)) {
                     rc.mineGold(mineLocation);
                 }
-                while (rc.canMineLead(mineLocation)) {
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
                     rc.mineLead(mineLocation);
                 }
             }
         }
-        // Also try to move randomly.
-        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+
+        int visionRadius = rc.getType().visionRadiusSquared;
+        MapLocation[] nearbyLocations = rc.getAllLocationsWithinRadiusSquared(me, visionRadius);
+
+        MapLocation targetLocation = null;
+        int distanceToTarget = Integer.MAX_VALUE;
+
+        for (MapLocation tryLocation : nearbyLocations) {
+            if (rc.senseLead(tryLocation) > 1 || rc.senseGold(tryLocation) > 0) {
+                int distanceTo = me.distanceSquaredTo(tryLocation);
+                if (distanceTo < distanceToTarget) {
+                    targetLocation = tryLocation;
+                    distanceToTarget = distanceTo;
+                }
+            }
+        }
+
+        Direction dir;
+        if (targetLocation != null)
+            dir = me.directionTo(targetLocation);
+        else
+            dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+
         if (rc.canMove(dir)) {
             rc.move(dir);
             System.out.println("I moved!");
         }
     }
+
+
 }
--- src/ourRobot/Watchtower.java
+++ src/ourRobot/Watchtower.java
@@ -1,10 +1,18 @@
 package ourRobot;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class Watchtower {
     static void runWatchtower(RobotController rc) throws GameActionException{
-
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length > 0) {
+            MapLocation toAttack = enemies[0].location;
+            if (rc.canAttack(toAttack)) {
+                rc.attack(toAttack);
+            }
+        }
     }
 }

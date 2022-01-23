3241dfd54494ba33d93182259a32b6b857a83e92
laBruji <htromero66@gmail.com>
2022-01-22 19:52:32 -0500

    walking strategy

---
+++ src/ourRobot/Walk.java
@@ -0,0 +1,84 @@
+package ourRobot;
+
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.GameActionException;
+
+import java.util.Map;
+
+public class Walk {
+    static void walk(RobotController rc, MapLocation target) throws GameActionException {
+        MapLocation initLocation = rc.getLocation();
+        MapLocation currentLocation;
+        MapLocation idealLocation = initLocation;
+        Direction initDir = initLocation.directionTo(target);
+        Direction currentDir = initDir;
+        Direction dirToTarget;
+        int initDistance = initLocation.distanceSquaredTo(target);
+        int minDistance = initDistance;
+        int currentDistance;
+
+
+        // if there are no obstacles, move towards target
+        if (rc.canMove(initDir)) {
+            rc.move(initDir);
+            return;
+        }
+
+        // move along obstacle
+        while(!rc.canMove(currentDir)){
+            currentDir = currentDir.rotateRight();
+        }
+        rc.move(currentDir);
+        currentLocation = rc.getLocation();
+
+        // next step
+        while(!currentLocation.equals(initLocation)) {
+            if (rc.canMove(currentDir.rotateLeft())) {
+                currentDir = currentDir.rotateLeft();
+
+            } else {
+                currentDir = currentDir.rotateRight();
+                while(!rc.canMove(currentDir)){
+                    currentDir = currentDir.rotateRight();
+                }
+            }
+            rc.move(currentDir);
+            currentLocation = rc.getLocation();
+            currentDistance = currentLocation.distanceSquaredTo(target);
+            dirToTarget = currentLocation.directionTo(target);
+            if(currentDistance < minDistance && dirToTarget.equals(initDir)) {
+                minDistance = currentDistance;
+                idealLocation = currentLocation;
+            }
+        }
+
+        currentDir = initDir;
+        while(!rc.canMove(currentDir)){
+            currentDir = currentDir.rotateRight();
+        }
+        rc.move(currentDir);
+        currentLocation = rc.getLocation();
+
+
+        while(!currentLocation.equals(idealLocation)){
+            if (rc.canMove(currentDir.rotateLeft())) {
+                currentDir = currentDir.rotateLeft();
+
+            } else {
+                currentDir = currentDir.rotateRight();
+                while(!rc.canMove(currentDir)){
+                    currentDir = currentDir.rotateRight();
+                }
+            }
+            rc.move(currentDir);
+            currentLocation = rc.getLocation();
+        }
+
+        if(rc.canMove(initDir)){
+            rc.move(initDir);
+        }
+
+    }
+}
